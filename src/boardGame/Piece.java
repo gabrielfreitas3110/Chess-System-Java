f1a53a508007e584e96af9612f0202b0bb842210
Gariel Augusto <gabriel.freitas3110@gmail.com>
2021-07-24 12:16:44 -0300

    Possible moves of a piece

--- src/boardGame/Piece.java
+++ src/boardGame/Piece.java
@@ -1,6 +1,6 @@
 package boardGame;
 
-public class Piece {
+public abstract class Piece {
 	protected Position position;
 	private Board board;
 	
@@ -10,5 +10,22 @@ public class Piece {
 
 	protected Board getBoard() {
 		return board;
+	}
+	
+	public abstract boolean[][] possibleMoves();
+	
+	public boolean possibleMove(Position position) {
+		return possibleMoves()[position.getRow()][position.getColumn()];
+	}
+	
+	public boolean isThereAnyPossibleMove() {
+		boolean[][] matriz = possibleMoves();
+		for(int i=0; i<matriz.length; i++) {
+			for(int j=0; j<matriz.length; j++) {
+				if(matriz[i][j])
+					return true;
+			}
+		}
+		return false;
 	}
 }
--- src/chess/pieces/Peon.java
+++ src/chess/pieces/Peon.java
@@ -14,4 +14,10 @@ public class Peon extends ChessPiece{
 	public String toString() {
 		return("P");
 	}
+
+	@Override
+	public boolean[][] possibleMoves() {
+		boolean[][] matriz = new boolean[getBoard().getRows()][getBoard().getColumns()];
+		return matriz;
+	}
 }
